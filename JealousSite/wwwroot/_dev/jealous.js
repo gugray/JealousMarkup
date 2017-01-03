b9e1b95073a9988466a1b0709e557adc3b022c55
ug <zydeodict@gmail.com>
2017-01-03 19:14:01 +0100

    WIP (responsive)

--- JealousSite/wwwroot/_dev/jealous.js
+++ JealousSite/wwwroot/_dev/jealous.js
@@ -6,19 +6,6 @@ var JM = JM || {};
 JM.site = (function () {
 
   $(document).ready(function () {
-    // Show debug info
-    //$("body").append("<div class='debug'></div>");
-
-    // Responsive layout
-    $(window).resize(onResize);
-    onResize();
   });
 
-  function onResize() {
-    var width = $("html").innerWidth();
-    $(".debug").html("Wnd: " + width);
-    if (width >= 800) { $("html").removeClass("smaller"); $("html").removeClass("mobile"); }
-    else if (width >= 620 && width < 800) { $("html").addClass("smaller"); $("html").removeClass("mobile"); }
-    else { $("html").addClass("smaller"); $("html").addClass("mobile"); }
-  }
 })();
